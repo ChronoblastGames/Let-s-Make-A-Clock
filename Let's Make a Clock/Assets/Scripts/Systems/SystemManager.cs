881346e5e2bfffda7526136047470d418c9bec41
Chron <ChronoblastGamesOfficial@gmail.com>
2016-12-07 12:07:38 -0500

    Initial Commit of Project Files.

---
+++ Let's Make a Clock/Assets/Scripts/Systems/SystemManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+    [Header("System")]
+    [HideInInspector]
+    public DateTime systemTime;
+    [HideInInspector]
+    public TimeSpan systemTimeSpan;
+
+    [Header("Season Booleans")]
+    public bool isSpring;
+    public bool isSummer;
+    public bool isFall;
+    public bool isWinter;
+
+    private void Update()
+    {
+        GetSystemDateTime();
+    }
+
+    void GetSystemDateTime()
+    {
+        systemTime = DateTime.Now;
+        systemTimeSpan = systemTime.TimeOfDay;
+    }
+
+    void ManageDate()
+    {
+
+    }
+
+    void ManageTime()
+    {
+
+    }
+}
