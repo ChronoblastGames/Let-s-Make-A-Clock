bfd32040b2f7b21b0a907a4fb1c32310b927eeec
Chron <ChronoblastGamesOfficial@gmail.com>
2016-12-07 14:30:06 -0500

    Started working on TIme/Day/Year logic.

--- Let's Make a Clock/Assets/Scripts/Systems/SystemManager.cs
+++ Let's Make a Clock/Assets/Scripts/Systems/SystemManager.cs
@@ -11,6 +11,22 @@ public class SystemManager : MonoBehaviour
     [HideInInspector]
     public TimeSpan systemTimeSpan;
 
+    [Header("Date")]
+    public string currentDate;
+    public string dayOfTheYear;
+
+    [Header("Time")]
+    public string currentTime;
+
+    [Header("Calendar Booleans")]
+    public bool isLeapYear;
+
+    [Header("Time Of Day Booleans")]
+    public bool isMorning;
+    public bool isAfternoon;
+    public bool isEvening;
+    public bool isNight;
+
     [Header("Season Booleans")]
     public bool isSpring;
     public bool isSummer;
@@ -20,6 +36,11 @@ public class SystemManager : MonoBehaviour
     private void Update()
     {
         GetSystemDateTime();
+
+        ManageDate();
+        ManageTime();
+
+        //Debug.Log
     }
 
     void GetSystemDateTime()
@@ -30,11 +51,79 @@ public class SystemManager : MonoBehaviour
 
     void ManageDate()
     {
+        currentDate = systemTime.Date.ToShortDateString();
 
+        if (CheckForSeason(systemTime.DayOfYear) == 1)
+        {
+            isSpring = false;
+            isSummer = false;
+            isFall = false;
+            isWinter = true;
+        }
+        else if (CheckForSeason(systemTime.DayOfYear) == 2)
+        {
+            isSpring = true;
+            isSummer = false;
+            isFall = false;
+            isWinter = false;
+        }
+        else if (CheckForSeason(systemTime.DayOfYear) == 3)
+        {
+            isSpring = false;
+            isSummer = true;
+            isFall = false;
+            isWinter = false;
+        }
+        else if (CheckForSeason(systemTime.DayOfYear) == 4)
+        {
+            isSpring = false;
+            isSummer = false;
+            isFall = true;
+            isWinter = false;
+        }
+        else
+        {          
+            isSpring = false;
+            isSummer = false;
+            isFall = false;
+            isWinter = true;
+        }
+
+        dayOfTheYear = systemTime.DayOfYear.ToString();
     }
 
     void ManageTime()
     {
+        currentTime = systemTime.ToShortTimeString();
+    }
 
+    private int CheckForSeason(int currentDayOfYear)
+    {
+        if (currentDayOfYear < 59) //If Before December 1 and Feb 28, return Winter;
+        {
+            return 1;
+        }
+        else if (currentDayOfYear < 151) //If Before March 1 and May 31, return Spring;
+        {
+            return 2;
+        }
+        else if (currentDayOfYear < 243) //If Between June 1 and August 31, return Summer;
+        {
+            return 3;
+        }
+        else if (currentDayOfYear < 334) //If Between September 1 and November 30, return Fall;
+        {
+            return 4;
+        }
+
+        else //Otherwise return Winter;
+        {
+            return 1;
+        }
+    }
+
+    private int CheckForTimeOfDay(int currentTimeOfDay)
+    {
+        return 1;
     }
 }
---
+++ Let's Make a Clock/Assets/Scripts/Systems/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private SystemManager systemManager;
+
+    [Header("UI Current Time")]
+    public Text currentTimeText;
+
+    [Header("UI Current Date")]
+    public Text currentDateText;
+
+	void Start ()
+    {
+        systemManager = GameObject.FindGameObjectWithTag("Clock").GetComponent<SystemManager>();
+	}
+	
+	void Update ()
+    {
+        ManageDateText();
+        ManageTimeText();
+	}
+
+    void ManageDateText()
+    {
+        currentDateText.text = systemManager.currentDate;
+    }
+
+    void ManageTimeText()
+    {
+        currentTimeText.text = systemManager.currentTime;
+    }
+}
